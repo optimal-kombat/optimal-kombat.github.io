3490a536311ca6f07b2f52e52b2f70e50b37e017
Oscar Kelly <ogkelly0121@gmail.com>
2025-06-16 20:06:00 +0100

    Add .nojekyll and update output for GitHub Pages

--- publishconf.py
+++ publishconf.py
@@ -11,10 +11,11 @@ THEME = 'themes/neon-chaos'
 DEFAULT_PAGINATION = 10
 RELATIVE_URLS = True
 
-STATIC_PATHS = ['extra/custom.css']
+STATIC_PATHS = ['extra/custom.css', 'extra/.nojekyll']
 
 EXTRA_PATH_METADATA = {
     'extra/custom.css': {'path': 'theme/css/custom.css'},
+    'extra/.nojekyll': {'path': '.nojekyll'},
 }
 
 SITEURL = "https://optimal-kombat.github.io"
